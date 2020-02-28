71307daed77ee0038c04e2f94c08baaeceb48e91
BrandonMGH <BrandonMGH@Yahoo.com>
2020-02-27 22:51:11 -0800

    got a working version of loopback with react on the client side up and running again.  Will set it up with Heroku tomorrow

---
+++ Client-Store-CRM/client/Index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export const Index = () => {
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
+ReactDOM.render(<Index />, document.getElementById("root"))
